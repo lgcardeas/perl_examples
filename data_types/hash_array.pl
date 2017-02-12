ad8b737d3b88a78c0e737fff7f518dea999c43d9
Luis Ernesto <lgcardenas910526@gmail.com>
2017-02-12 08:33:28 -0500

    Working with data types. This case hash array

---
+++ data_types/hash_array.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+#
+#	Examples how using hash array....
+#
+
+%translator = (
+		"year"  => "año",
+		"month" => "mes",
+		"day"   => "dia"		
+	);
+
+print "$translator{year}\n";
+print "$translator{month}\n";
+print "$translator{day}\n";
